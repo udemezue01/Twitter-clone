7937f7500467a1d273b7677313ebae40ae1d862d
Udemezue John <johnzeus14@gmail.com>
2023-07-09 17:54:17 +0100

    Added Tailwids CSS and configured

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1 class="text-2xl text-gray-900 text-center ">Welcome to Twitter</h1>
+    <h1 class="text-2xl text-blue-400 text-center ">Welcome to Twitter</h1>
   </div>
 </template>
 
