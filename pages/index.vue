e6849a73b5343a9d2b0fc23a3646bb8a774148be
Udemezue John <johnzeus14@gmail.com>
2023-07-15 20:11:45 +0100

    Added grids to landing page

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,14 @@
 <template>
-  <div>
-    <h1 class="text-2xl text-blue-400 text-center ">Welcome to Twitter</h1>
+  <div class="container mx-auto px-4 grid grid-cols-3">
+    <div>
+      <h1 class="text-2xl text-blue-400 text-center "> First Section </h1>
+    </div>
+    <div>
+      <h1 class="text-2xl text-blue-400 text-center "> Second Section</h1>
+    </div>
+    <div>
+      <h1 class="text-2xl text-blue-400 text-center ">Third Section</h1>
+    </div>
   </div>
 </template>
 
